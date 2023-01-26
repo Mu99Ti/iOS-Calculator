0b5e64c16e261a21e2fd896e8297ea33ecb59e4e
Muhammad <87659760+Mu99Ti@users.noreply.github.com>
2023-01-26 21:20:41 +0330

    Initial Commit

---
+++ iOS-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Calculator
+//
+//  Created by Muhamad Talebi on 1/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
